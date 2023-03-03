5c8e460587018e0888f9a57c48e4cee39a20785d
ohtagaki <sweetlife0815hiikor@gmail.com>
2023-03-03 23:15:14 +0900

    ゼロから始めるrust 2.1.7まで完了

--- rust_zero/chapter02/section01/src/main.rs
+++ rust_zero/chapter02/section01/src/main.rs
@@ -28,6 +28,60 @@ fn main() {
     println!("{}", 240 >= 18);
     println!("{}", 2023 == 34);
 
+    // 浮動小数点型
+    // 加減乗除
+    println!("{}", 1.52 + 0.435);
+    println!("{}", 3.6 - 0.96);
+    println!("{}", 5.6 * 14.6);
+    println!("{}", 58.0 / 41.5);
+    println!("{}", 73.0 % 23.4);
+    // 比較
+    println!("{}", 1.45 < 0.53);
+    println!("{}", 2.3 <= 1.2);
+    println!("{}", 5.6 > 14.2);
+    println!("{}", 58.2 >= 41.5);
+    println!("{}", 73.5 == 23.4);
+
+    // ビット演算
+    let player: u16 = 
+        1 | 
+        (1 << 1) |
+        (3 << 2);
+
+    if player & 1 != 0 {
+        println!("毒状態");
+    }
+
+    if player & (1 << 1)  != 0 {
+        println!("攻撃力アップ状態");
+    }
+    println!("playerの値 = {player}");
+    let hp = (player & 0xfffc) >> 2; // ビットマスクを用いた計算
+    println!("残り体力 = {hp}");
+
+    // 参照と破壊的代入の例
+    let mut n: u64 = 100; // nは破壊的代入可能
+    let a: &u64 = &n; // aにnの不変参照を代入
+    // aを参照外ししした値とアドレスを表示
+    println!("*a = {}, address = {:p}", *a, a);
+
+    let b: &mut u64 = &mut n; // bにnの可変参照を代入
+    *b = 300;
+    println!("*n = {n}");
+
+    // 配列型とスライス
+    let arr: [u32; 4] = [2, 4, 6, 8]; // 配列を定義
+    println!("{}, {}, {}, {}", arr[0],arr[1],arr[2],arr[3]);
+    let slic: &[u32] = &arr[2..4]; //スライスを取得
+    println!("{:?}", slic);
+
+    // 文字と文字列の型
+    let a: &str = "hello"; // 文字列スライス
+    let mut b: String = a.to_string(); // Stringに変換
+    b += " , world!  ";
+    let c: &str = b.trim(); // 前後の空白文字を取り除いたスライスを取得
+    println!("{c}");
+
 
 }
 
